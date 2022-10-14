a96e4d582799497d7f09f015c80db7da4050400d
Kamala <kamala123mohan@gmail.com>
2022-10-15 00:38:20 +0530

    Created server and dbconfig file

--- configs/server.config.js
+++ configs/server.config.js
@@ -1,9 +1,12 @@
+//The first thing that gets loaded is the "process.env", in that we have 
+//an env called the "NODE_ENV", so thats not equal to the production
+//which means its in the dev envrionment, so then we call the "dotenv" module.
+
 if(process.env.NODE_ENV != 'production'){
     require('dotenv').config();
 }
+
+//This can be exported and used by other files 
 module.exports = {
     PORT:process.env.PORT
 }
-
-
-
